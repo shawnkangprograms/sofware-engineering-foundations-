a7bef50935edd0c31f00a9182c7fe8a2729b1be5
Shawn Njoroge <njorogeshawn4@gmail.com>
2026-08-17 20:49:39 +0300

    first commit

---
+++ week01/day1.py
@@ -0,0 +1,6 @@
+first_name = input("Hello there! Enter your first name: ")
+last_name = input("I'll also need your last name: ")
+print("Thanks for your input " + first_name + " " + last_name + "!")
+
+age = int(input("Hi " + first_name + "! Could you input your age for no reason at all? " ))
+print("I will now guess your age " + last_name +". You're " + str(age) +" years old!")
